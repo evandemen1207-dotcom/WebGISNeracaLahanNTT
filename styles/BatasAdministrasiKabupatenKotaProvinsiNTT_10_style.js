d485674df0a829a197cc0bd728fdc421b5f872b3
evandemen1207-dotcom <evandemen1207@gmail.com>
2026-09-10 16:18:45 +0700

    Add files via upload

---
+++ styles/BatasAdministrasiKabupatenKotaProvinsiNTT_10_style.js
@@ -0,0 +1,33 @@
+var size = 0;
+var placement = 'point';
+
+var style_BatasAdministrasiKabupatenKotaProvinsiNTT_10 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10.4px \'Calibri\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("WADMKK") !== null) {
+        labelText = String(feature.get("WADMKK"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
